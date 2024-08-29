56d13837072b5ef52b218a2139f86a80a0fb7894
David Ruiz <druiz0073@gmail.com>
2024-08-29 00:17:32 -0300

    aprendimos a usar el if

--- prueba.py
+++ prueba.py
@@ -1,1 +1,11 @@
-print("hola mundo")
+from unicodedata import numeric
+
+print("🍆bienvenidos a programacion la concha de tu madre🍆")
+a=input("tenes notebook para el cursado?")
+if a == "si":
+    print("bien")
+else:
+   if a == "no":
+       print("comprate pibe")
+   else:
+       print("🥚sanso comeme los huevardos🥚")
