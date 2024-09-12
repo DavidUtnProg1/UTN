2c51d4d5982b8067b647d5f6417dbc7c5b38dafc
david <druiz0073@gmail.com>
2024-09-12 02:28:22 -0300

    unidad 4.1

---
+++ Ejercicio2.py
@@ -0,0 +1,5 @@
+#Leer dos números reales, calcular e imprimir los dos posibles cocientes entre ellos
+num1=int(input("el primer valor"))
+num2=int(input("ingrese el segundo valor"))
+print(num1/num2)
+print(num2/num1)
